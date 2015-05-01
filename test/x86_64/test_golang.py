f25dbb0dc6bcc931dbac421ea98a7f2b5873b39c
Marat Dukhan <maratek@gmail.com>
2015-05-01 18:20:34 -0400

    Test: avoid register allocation in Golang tests

--- test/x86_64/test_golang.py
+++ test/x86_64/test_golang.py
@@ -25,18 +25,18 @@ class ReturnIntegerArgument(unittest.TestCase):
         n_arg = Argument(uint32_t)
 
         with Function("return_int_arg", (n_arg,), uint32_t) as function:
-            n = GeneralPurposeRegister32()
+            n = ebx
             LOAD.ARGUMENT(n, n_arg)
 
-            MOV(eax, n)
+            STORE.RESULT(n)
             RETURN()
 
         code = function.finalize(abi.golang_amd64_abi).format("go")
         ref_code = """
 // func return_int_arg(n uint32) uint32
 TEXT \xc2\xB7return_int_arg(SB),4,$0-8
-    MOVL n+0(FP), AX
-    MOVL AX, AX
+    MOVL n+0(FP), BX
+    MOVL BX, ret+4(FP)
     RET
 """
         assert equal_codes(code, ref_code), "Unexpected Golang code:\n" + code
@@ -49,23 +49,24 @@ class ComputeIntegerSum(unittest.TestCase):
         y_arg = Argument(uint32_t)
 
         with Function("integer_sum", (x_arg, y_arg), uint32_t) as function:
-            x = GeneralPurposeRegister32()
+            x = ecx
             LOAD.ARGUMENT(x, x_arg)
-            y = GeneralPurposeRegister32()
+            y = r8d
             LOAD.ARGUMENT(y, y_arg)
 
             ADD(x, y)
-            MOV(eax, x)
+
+            STORE.RESULT(x)
             RETURN()
 
         code = function.finalize(abi.golang_amd64_abi).format("go")
         ref_code = """
 // func integer_sum(x uint32, y uint32) uint32
 TEXT \xc2\xB7integer_sum(SB),4,$0-12
-    MOVL x+0(FP), AX
-    MOVL y+4(FP), R10
-    ADDL R10, AX
-    MOVL AX, AX
+    MOVL x+0(FP), CX
+    MOVL y+4(FP), R8
+    ADDL R8, CX
+    MOVL CX, ret+8(FP)
     RET
 """
         assert equal_codes(code, ref_code), "Unexpected Golang code:\n" + code
