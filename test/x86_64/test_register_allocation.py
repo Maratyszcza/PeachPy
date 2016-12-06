bbe6218599260c52d4341a89c69e3073c66ca60d
Marat Dukhan <maratek@gmail.com>
2016-12-06 12:06:03 +0300

    Test: register allocation with conflicts between virtual and physical registers

---
+++ test/x86_64/test_register_allocation.py
@@ -0,0 +1,64 @@
+import unittest
+from test import equal_codes
+from peachpy import *
+from peachpy.x86_64 import *
+
+
+class TestExplicitRegisterInputConflict(unittest.TestCase):
+    def runTest(self):
+        with Function("explicit_reg_input", (), uint64_t) as function:
+            reg_tmp = GeneralPurposeRegister64()
+            MOV(reg_tmp, 42)
+            MOV([reg_tmp], rax)
+            RETURN(reg_tmp)
+
+        listing = function.finalize(abi.goasm_amd64_abi).format("go")
+        ref_listing = """
+// func explicit_reg_input() uint64
+TEXT \xc2\xB7explicit_reg_input(SB),4,$0-8
+   	MOVQ $42, BX
+   	MOVQ AX, 0(BX)
+   	MOVQ BX, ret+0(FP)
+   	RET
+"""
+        assert equal_codes(listing, ref_listing), "Unexpected PeachPy listing:\n" + listing
+
+
+class TestExplicitRegisterOutputConflict(unittest.TestCase):
+    def runTest(self):
+        with Function("explicit_reg_output", (), uint64_t) as function:
+            reg_tmp = GeneralPurposeRegister64()
+            MOV(reg_tmp, 1)
+            MOV(rax, 2)
+            RETURN(reg_tmp)
+
+        listing = function.finalize(abi.goasm_amd64_abi).format("go")
+        ref_listing = """
+// func explicit_reg_output() uint64
+TEXT \xc2\xB7explicit_reg_output(SB),4,$0-8
+   	MOVQ $1, BX
+   	MOVQ $2, AX
+   	MOVQ BX, ret+0(FP)
+   	RET
+"""
+        assert equal_codes(listing, ref_listing), "Unexpected PeachPy listing:\n" + listing
+
+
+class TestImplicitRegisterConflict(unittest.TestCase):
+    def runTest(self):
+        with Function("implicit_reg", (), uint64_t) as function:
+            reg_tmp = GeneralPurposeRegister64()
+            MOV(reg_tmp, 42)
+            CPUID()
+            RETURN(reg_tmp)
+
+        listing = function.finalize(abi.goasm_amd64_abi).format("go")
+        ref_listing = """
+// func implicit_reg() uint64
+TEXT \xc2\xB7implicit_reg(SB),4,$0-8
+	MOVQ $42, DI
+	CPUID
+	MOVQ DI, ret+0(FP)
+	RET
+"""
+        assert equal_codes(listing, ref_listing), "Unexpected PeachPy listing:\n" + listing
