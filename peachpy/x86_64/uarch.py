2375e9781340bb33bcc15f9021664abfb159ebb6
Marat Dukhan <maratek@gmail.com>
2017-04-02 19:27:58 -0400

    x86-64: update ISA support in uArchs

--- peachpy/x86_64/uarch.py
+++ peachpy/x86_64/uarch.py
@@ -86,13 +86,13 @@ class Microarchitecture:
 
 default = Microarchitecture('Default', isa.default,
                             alu_width=128, fpu_width=128, load_with=128, store_width=128)
-prescott = Microarchitecture('Prescott', (isa.cmov, isa.sse3),
+prescott = Microarchitecture('Prescott', (isa.cmov, isa.sse3, isa.clflush),
                              alu_width=64, fpu_width=64, load_with=64, store_width=64)
-conroe = Microarchitecture('Conroe', (isa.cmov, isa.mmx_plus, isa.ssse3),
+conroe = Microarchitecture('Conroe', (isa.cmov, isa.mmx_plus, isa.ssse3, isa.clflush),
                            alu_width=128, fpu_width=128, load_with=128, store_width=128)
-penryn = Microarchitecture('Penryn', (isa.cmov, isa.mmx_plus, isa.sse4_1),
+penryn = Microarchitecture('Penryn', (isa.cmov, isa.mmx_plus, isa.sse4_1, isa.clflush),
                            alu_width=128, fpu_width=128, load_with=128, store_width=128)
-nehalem = Microarchitecture('Nehalem', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt),
+nehalem = Microarchitecture('Nehalem', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt, isa.clflush),
                             alu_width=128, fpu_width=128, load_with=128, store_width=128)
 sandy_bridge = Microarchitecture('Sandy Bridge', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt, isa.avx),
                                  alu_width=128, fpu_width=256, load_with=256, store_width=128)
@@ -105,15 +105,14 @@ broadwell = Microarchitecture('Broadwell', (isa.cmov, isa.mmx_plus, isa.sse4_2, 
                                             isa.lzcnt, isa.prefetchw, isa.movbe, isa.bmi2, isa.adx),
                               alu_width=256, fpu_width=256, load_with=256, store_width=256)
 skylake = Microarchitecture('Skylake', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt, isa.f16c, isa.fma3, isa.avx2,
-                                        isa.lzcnt, isa.prefetchw, isa.movbe, isa.bmi2, isa.adx),
+                                        isa.lzcnt, isa.prefetchw, isa.clflushopt, isa.movbe, isa.bmi2, isa.adx),
                             alu_width=256, fpu_width=256, load_with=256, store_width=256)
 skylake_xeon = Microarchitecture('Skylake Xeon', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt, isa.f16c, isa.fma3,
-                                                  isa.lzcnt, isa.prefetchw, isa.movbe, isa.bmi2, isa.adx,
+                                                  isa.lzcnt, isa.prefetchw, isa.clflushopt, isa.movbe, isa.bmi2, isa.adx,
                                                   isa.avx512bw, isa.avx512dq, isa.avx512vl, isa.avx512cd),
-                                 # TODO: update EU width when SKX is out
                                  alu_width=512, fpu_width=512, load_with=512, store_width=512)
 cannonlake = Microarchitecture('Cannonlake', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt, isa.f16c, isa.fma3,
-                                              isa.lzcnt, isa.prefetchw, isa.movbe, isa.bmi2, isa.adx,
+                                              isa.lzcnt, isa.prefetchw, isa.clflushopt, isa.movbe, isa.bmi2, isa.adx,
                                               isa.avx512bw, isa.avx512dq, isa.avx512vl, isa.avx512cd,
                                               isa.avx512ifma, isa.avx512vbmi),
                                # TODO: update EU width when CNL is out
@@ -122,56 +121,58 @@ knights_landing = Microarchitecture('Knights Landing',
                                     (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.popcnt, isa.f16c, isa.fma3,
                                      isa.lzcnt, isa.prefetchw, isa.movbe, isa.bmi2, isa.adx,
                                      isa.avx512cd, isa.avx512cd, isa.avx512er),
-                                    # TODO: update EU width when KNL is out
                                     alu_width=512, fpu_width=512, load_with=512, store_width=512)
 k8 = Microarchitecture('K8', (isa.cmov, isa.mmx_plus, isa.three_d_now_plus, isa.sse2,
-                              isa.prefetch, isa.prefetchw),
+                              isa.prefetch, isa.prefetchw, isa.clflush),
                        alu_width=64, fpu_width=64, load_with=64, store_width=64)
 k10 = Microarchitecture('K10', (isa.cmov, isa.mmx_plus, isa.three_d_now_plus, isa.sse4a,
-                                isa.prefetch, isa.prefetchw, isa.popcnt, isa.lzcnt),
+                                isa.prefetch, isa.prefetchw, isa.clflush, isa.popcnt, isa.lzcnt),
                         alu_width=128, fpu_width=128, load_with=128, store_width=64)
 bulldozer = Microarchitecture('Bulldozer', (isa.cmov, isa.mmx_plus, isa.sse4a, isa.avx, isa.xop, isa.fma4,
-                                            isa.prefetch, isa.prefetchw, isa.aes, isa.pclmulqdq, isa.lzcnt, isa.popcnt),
+                                            isa.prefetch, isa.prefetchw, isa.clflush,
+                                            isa.aes, isa.pclmulqdq, isa.lzcnt, isa.popcnt),
                               alu_width=128, fpu_width=128, load_with=128, store_width=128)
 piledriver = Microarchitecture('Piledriver', (isa.cmov, isa.mmx_plus, isa.sse4a, isa.sse4_2,
                                               isa.avx, isa.xop, isa.fma4, isa.fma3, isa.f16c, isa.aes, isa.pclmulqdq,
-                                              isa.prefetch, isa.prefetchw, isa.lzcnt, isa.popcnt, isa.bmi, isa.tbm),
+                                              isa.prefetch, isa.prefetchw, isa.clflush,
+                                              isa.lzcnt, isa.popcnt, isa.bmi, isa.tbm),
                                alu_width=128, fpu_width=128, load_with=128, store_width=128)
 steamroller = Microarchitecture('Steamroller', (isa.cmov, isa.mmx_plus, isa.sse4a, isa.sse4_2,
                                                 isa.avx, isa.xop, isa.fma4, isa.fma3, isa.f16c, isa.aes, isa.pclmulqdq,
-                                                isa.prefetch, isa.prefetchw, isa.lzcnt, isa.popcnt, isa.bmi, isa.tbm),
+                                                isa.prefetch, isa.prefetchw, isa.clflush,
+                                                isa.lzcnt, isa.popcnt, isa.bmi, isa.tbm),
                                 alu_width=128, fpu_width=256, load_with=256, store_width=128)
 excavator = Microarchitecture('Excavator', (isa.cmov, isa.mmx_plus, isa.sse4a, isa.xop, isa.fma4, isa.fma3, isa.f16c,
-                                            isa.avx2, isa.aes, isa.pclmulqdq, isa.rdrand, isa.prefetch, isa.prefetchw,
+                                            isa.avx2, isa.aes, isa.pclmulqdq, isa.rdrand,
+                                            isa.prefetch, isa.prefetchw, isa.clflush,
                                             isa.lzcnt, isa.popcnt, isa.bmi2, isa.tbm),
                               alu_width=256, fpu_width=256, load_with=256, store_width=128)
-# TODO: update info when Zen is out
 zen = Microarchitecture('Zen', (isa.cmov, isa.mmx_plus, isa.fma4, isa.fma3, isa.f16c, isa.avx2,
                                 isa.aes, isa.pclmulqdq, isa.rdseed, isa.sha,
-                                isa.prefetch, isa.prefetchw, isa.clzero,
+                                isa.prefetch, isa.prefetchw, isa.clflushopt, isa.clzero,
                                 isa.lzcnt, isa.popcnt, isa.bmi2, isa.adx),
                         alu_width=256, fpu_width=256, load_with=256, store_width=256)
-bonnell = Microarchitecture('Bonnell', (isa.cmov, isa.movbe, isa.mmx_plus, isa.ssse3),
+bonnell = Microarchitecture('Bonnell', (isa.cmov, isa.movbe, isa.mmx_plus, isa.ssse3, isa.clflush),
                             alu_width=128, fpu_width=64, load_with=128, store_width=128)
-saltwell = Microarchitecture('Saltwell', (isa.cmov, isa.movbe, isa.mmx_plus, isa.ssse3),
+saltwell = Microarchitecture('Saltwell', (isa.cmov, isa.movbe, isa.mmx_plus, isa.ssse3, isa.clflush),
                              alu_width=128, fpu_width=64, load_with=128, store_width=128)
-silvermont = Microarchitecture('Silvermont', (isa.cmov, isa.movbe, isa.popcnt, isa.mmx_plus, isa.sse4_2, isa.aes,
-                                              isa.pclmulqdq),
+silvermont = Microarchitecture('Silvermont', (isa.cmov, isa.movbe, isa.popcnt, isa.clflush,
+                                              isa.mmx_plus, isa.sse4_2, isa.aes, isa.pclmulqdq),
                                alu_width=128, fpu_width=64, load_with=128, store_width=128)
 airmont = Microarchitecture('Airmont', (isa.cmov, isa.movbe, isa.popcnt,
                                         isa.mmx_plus, isa.sse4_2,
                                         isa.aes, isa.pclmulqdq, isa.rdrand,
-                                        isa.prefetchw, isa.rdtscp),
+                                        isa.prefetchw, isa.clflush, isa.rdtscp),
                             alu_width=128, fpu_width=64, load_with=128, store_width=128)
 goldmont = Microarchitecture('Goldmont', (isa.cmov, isa.movbe, isa.popcnt, isa.adx,
-                                          isa.mmx_plus, isa.sse4_2, isa.prefetchw,
+                                          isa.mmx_plus, isa.sse4_2, isa.prefetchw, isa.clflushopt,
                                           isa.aes, isa.pclmulqdq, isa.rdseed, isa.sha,
                                           isa.rdtscp),
                              alu_width=128, fpu_width=64, load_with=128, store_width=128)
 bobcat = Microarchitecture('Bobcat', (isa.cmov, isa.mmx_plus, isa.ssse3, isa.sse4a,
-                                      isa.prefetch, isa.prefetchw, isa.lzcnt, isa.popcnt),
+                                      isa.prefetch, isa.prefetchw, isa.clflush, isa.lzcnt, isa.popcnt),
                            alu_width=64, fpu_width=64, load_with=64, store_width=64)
 jaguar = Microarchitecture('Jaguar', (isa.cmov, isa.mmx_plus, isa.sse4_2, isa.sse4a, isa.avx, isa.f16c,
-                                      isa.prefetch, isa.prefetchw, isa.lzcnt, isa.popcnt, isa.movbe,
+                                      isa.prefetch, isa.prefetchw, isa.clflush, isa.lzcnt, isa.popcnt, isa.movbe,
                                       isa.aes, isa.pclmulqdq),
                            alu_width=128, fpu_width=128, load_with=128, store_width=128)
