c818fd7f7b2f487b079dcd5d233cd7c6edbbe730
Alfredo Tupone <tupone@gentoo.org>
2022-05-15 20:03:39 +0200

    Update setup.py
    Consider the case where self.distribution.package_dir is an empty dictionary

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class GenerateInstructions(Command):
     def run(self):
         # package_dir may be None, in that case use the current directory.
         import os
-        if self.distribution.package_dir is None:
+        if not self.distribution.package_dir:
             src_dir = os.getcwd()
         else:
             src_dir = os.path.abspath(self.distribution.package_dir[""])
